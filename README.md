2e7aac4866191046118347248e62356a9821f10a
Om3rCitak <mail@omercitak.com>
2016-07-04 19:20:29 +0300

    first commit baby

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # rhymer
-Find the similar keywords and make rhyme!
+rhymer, find the nearest word is a word using levenshtein algorithm helps make the rhyme songwriter.
+
+# levenshtein algorithm 
+
+https://en.wikipedia.org/wiki/Levenshtein_distance
+
+# install
+if not installed "requests" library
+```sh
+$ pip install requests
+``` 
+after installing "requests" library
+```sh
+$ git clone https://github.com/Om3rCitak/rhymer.git
+``` 
+
+# usage
+```sh
+$ python rhymer.py -k araba
+``` 
+![usage rhymer](rhymer.png)
+
+# wordlist
+all turkish words approved by the Türk Dil Kurumu and the recursive brute-force techniques save the wordlist_tdk.txt file.
+
+if you want to update the wordlist
+```sh
+$ cp wordlist_tdk.txt wordlist_tdk_old.txt
+$ python create_wordlist.py
+```
+
+this process will take slightly longer
+
+2k16, omercitak.com
---
+++ rhymer.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+import sys, argparse
+
+######################################################
+#|--------------------------------------------------|#
+#|													|#
+#|		coded by om3rcitak www.omercitak.com		|#
+#|													|#
+#|--------------------------------------------------|#
+######################################################
+
+wordlist = "wordlist_tdk.txt"
+max_dest = 1
+
+def usage():
+	print('usage: rhymer.py [-h] [-k KEYWORD]\n')
+	print('optional arguments:')
+	print('  -h, --help            show this help message and exit')
+	print('  -k KEYWORD, --keyword KEYWORD')
+
+def levenshtein(string1, string2):
+
+	if len(string1) < len(string2):
+		return levenshtein(string2, string1)
+
+	if len(string2) == 0:
+		return len(string1)
+
+	previous = range(len(string2) + 1)
+
+	for i, current1 in enumerate(string1):
+		current = [i + 1]
+		for j, current2 in enumerate(string2):
+			insertions = previous[j + 1] + 1
+			deletions = current[j] + 1
+			substitutions = previous[j] + (current1 != current2)
+			current.append(min(insertions, deletions, substitutions))
+		previous = current
+
+	return previous[-1]
+
+def main(argv):
+
+	parser = argparse.ArgumentParser()
+	parser.add_argument('-k', '--keyword')
+	args = parser.parse_args()
+	keyword = args.keyword
+	
+	if keyword == None:
+		usage()
+		sys.exit(0)
+
+	file = open(wordlist)
+
+	for line in file:
+		dest = levenshtein(keyword, line.rstrip())
+		if dest <= max_dest:
+			print(line.rstrip())
+
+	file.close()
+
+if __name__ == "__main__":
+	main(sys.argv[1:])
